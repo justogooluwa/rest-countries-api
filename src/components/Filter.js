80b635c194608ba397b34bf0df3bc16f7f0edee1
ogooluwa <ogooluwaakinleye29@gmail.com>
2024-08-27 07:52:59 -0700

    commit

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -23,7 +23,7 @@ const Filter = () => {
   };
 
   useEffect(() => {
-    fetchCountryData();
+    handleSearch();
   }, []);
 
   const handleSearchChange = (e) => {
@@ -67,7 +67,7 @@ const Filter = () => {
           >
             <option value='Filter by region'>Filter by region</option>
             <option value='Africa'>Africa</option>
-            <option value='America'>America</option>
+            <option value='Americas'>America</option>
             <option value='Asia'>Asia</option>
             <option value='Europe'>Europe</option>
             <option value='Oceania'>Oceania</option>
